c0bc7d388b2fa49d149080cbd593b3b149d9d4a6
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2021-11-06 00:40:07 +0300

    feat: add service and manager to work with api

---
+++ src/TMS.DotNet.Group.1.Kaloska.Homework-9.Logic/Services/NewsApiService.cs
@@ -0,0 +1,29 @@
+﻿using Data.Models;
+using Logic.Constants;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Logic.Services
+{
+    public class NewsApiService
+    {
+        private HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            return client;
+        }
+        public async Task<NewsResponse> GetNewsAsyns()
+        {
+            HttpClient client = GetClient();
+            var result = await client.GetAsync(CommonConstants.Web);
+            return JsonConvert.DeserializeObject<NewsResponse>(await result.Content.ReadAsStringAsync());
+        }
+    }
+}
