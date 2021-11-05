fe9fa4e49c9bfa0d06b279c8a9fb5aab89e05e8b
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2021-11-06 00:38:18 +0300

    refactor: remove  empty Class1

--- src/TMS.DotNet.Group.1.Kaloska.Homework-9.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TMS.DotNet.Group._1.Kaloska.Homework_9.Data
-{
-    public class Class1
-    {
-    }
-}
--- src/TMS.DotNet.Group.1.Kaloska.Homework-9.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TMS.DotNet.Group._1.Kaloska.Homework_9.Logic
-{
-    public class Class1
-    {
-    }
-}
