26d030bf33b7e6535746a424ba7c6f445959f333
s4ipakin <62203513+s4ipakin@users.noreply.github.com>
2021-11-07 22:03:53 +0300

    feat: add reflection to make the main UI class closed

--- src/TMS.DotNet.Group.1.Kaloska.Homework-9.Logic/Services/UiService.cs
+++ src/TMS.DotNet.Group.1.Kaloska.Homework-9.Logic/Services/UiService.cs
@@ -1,47 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Logic.Managers;
 using TMS.DotNet.Group._1.Kaloska.Homework_9.Logic.Managers;
+using TMS.DotNet.Group._1.Kaloska.Homework_9.Logic.Operations;
 using TMS.DotNet.Group._1.Kaloska.Homework_9.Logic.Services;
 
 namespace Logic.Services
 {
     public class UiService
     {
+        private static Dictionary<int, OperationBase> _operations = new Dictionary<int, OperationBase>();
+
+        public UiService()
+        {
+            var allOperations = Assembly.GetAssembly(typeof(OperationBase)).GetTypes()
+                .Where(t => typeof(OperationBase).IsAssignableFrom(t) && t.IsAbstract == false);
+
+            foreach (var operation in allOperations)
+            {
+                OperationBase newOperation = Activator.CreateInstance(operation) as OperationBase;
+                _operations.Add(newOperation.Id, newOperation);
+            }
+        }
+
         public async Task RunAsync()
         {
+            List<OperationBase> operationList = _operations.Values.OrderBy(x => x.Id).ToList();
             try
             {
                 var key = default(ConsoleKey);
                 while (key != ConsoleKey.Q)
                 {
-                    Console.WriteLine("What do you want to see?\n1 - News." +
-                        "\n2 - Weather forecast." +
-                        "\n3 - Currency rates.");
+                    Console.WriteLine("What do you want to see?");
+                    foreach (var operation in operationList)
+                    {
+                        Console.WriteLine($"{operation.Id} ..." + operation.Name);
+                    }
                     var isSuccess = int.TryParse(Console.ReadLine(), out int type);
                     if (isSuccess)
                     {
-                        switch (type)
-                        {
-                            case 1:
-                                {
-                                    await NewsApiServiceManager.ShowNewsAsync();
-                                    break;
-                                }
-                            case 2:
-                                {
-                                    await WeatherUiService.RunAsync();
-                                    break;
-                                }
-                            case 3:
-                                await CurrencyApiServiceManager.ShowCurrencyAsync();
-                                break;
-                            default:
-                                {
-                                    IncorrectChoice();
-                                    break;
-                                }
-                        }
+                        await _operations[type].ShowData();                      
                     }
                     else
                     {
